5013bf9f47450a56bb47bc721b7be9409a076498
Mohit Jain <66169458+mohitjain1911@users.noreply.github.com>
2024-09-03 18:16:55 +0530

    commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
-
 # Pull the Docker image from Docker Hub
-echo
-
+docker pull tsmohitjain/task_manager
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 abhishekf5/task_manager
