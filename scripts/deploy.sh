37edcc64483a3b51866b50dd59c8dee0172543b6
mohitjain1911 <mohitjain19113@gmail.com>
2024-09-05 15:17:58 +0530

    commit

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Convert line endings
-dos2unix /opt/codedeploy-agent/deployment-root/$eecd941b-7fd8-4280-a1a3-9d098c22f358/d-7BN7D7VV6/deployment-archive/scripts/start_container.sh
-dos2unix /opt/codedeploy-agent/deployment-root/$eecd941b-7fd8-4280-a1a3-9d098c22f358/d-7BN7D7VV6/deployment-archive/scripts/stop_container.sh
-
-# Execute the actual deployment scripts
-/opt/codedeploy-agent/deployment-root/$eecd941b-7fd8-4280-a1a3-9d098c22f358/d-7BN7D7VV6/deployment-archive/scripts/start_container.sh
