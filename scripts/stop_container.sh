78f82b0c559e165be99c170f737f7ab687e53303
mohitjain1911 <mohitjain19113@gmail.com>
2024-09-05 12:49:29 +0000

    COMMIT

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,5 +4,5 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
-containerid=`docker ps | awk —F " " '{print $1}'`
+containerid=`docker ps | awk -F " " '{print $1}'`
 docker rm -f $containerid
