325f7ae39b99dcbfc57ea55486f5ca953ae03f5c
mohitjain1911 <mohitjain19113@gmail.com>
2024-09-05 13:12:02 +0000

    Ports

--- app.py
+++ app.py
@@ -11,5 +11,7 @@ db = SQLAlchemy(app)
 # Import routes here to avoid circular imports
 from routes import *
 
+#if __name__ == '__main__':
+#   app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
